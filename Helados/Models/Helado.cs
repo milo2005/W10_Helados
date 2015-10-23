2285022c8d882c71a60588635dfef2f823f43087
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-10-23 18:24:33 -0500

    Enlace a datos OneShot

---
+++ Helados/Models/Helado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helados.Models
+{
+    public class Helado
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private string sabor;
+
+        public string Sabor
+        {
+            get { return sabor; }
+            set { sabor = value; }
+        }
+
+        private string precio;
+
+        public string Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+
+
+    }
+}
