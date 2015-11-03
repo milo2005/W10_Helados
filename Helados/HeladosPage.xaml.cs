6a9797b3df2edf0dcb010e64ba68af2ff69c6248
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-11-02 19:28:37 -0500

    Finalizacion de proyecto

--- Helados/HeladosPage.xaml.cs
+++ Helados/HeladosPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Helados.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,5 +29,45 @@ namespace Helados
         {
             this.InitializeComponent();
         }
+
+        public HeladoConverter HeladoC {
+            get {
+                HeladoConverter heladoC = App.Current.Resources["heladoConverter"] as HeladoConverter;
+                return heladoC;
+            }
+        }
+
+
+        private ObservableCollection<Helado> listaHelados;
+
+        public ObservableCollection<Helado> ListaHelados
+        {
+            get {
+
+                if (listaHelados == null)
+                {
+                    listaHelados = new ObservableCollection<Helado>();
+
+                    Helado vainilla = new Helado() { Nombre = "Vainilla Pasion", Precio = 7000, Sabor = "Vainilla", Color = Color.FromArgb(0xff, 0xff, 0x00, 0x00) };
+                    Helado lulo = new Helado() { Nombre = "Lulo Pasion", Precio = 7000, Sabor = "Lulo", Color = Color.FromArgb(0xff, 0x00, 0xff, 0x00) };
+                    Helado mandarina = new Helado() { Nombre = "Mandarina Pasion", Precio = 7000, Sabor = "Mandarina", Color = Color.FromArgb(0xff, 0xff, 0x44, 0x00) };
+
+                    listaHelados.Add(vainilla);
+                    listaHelados.Add(lulo);
+                    listaHelados.Add(mandarina);
+
+
+                }
+                return listaHelados; }
+            set { listaHelados = value; }
+        }
+
+
+
+
     }
+
+   
+
+
 }
